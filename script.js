e258dd88db8fe6cf4af930c57c5790371c784475
kellisha2 <kellisha.andrew2@gmail.com>
2024-11-16 20:08:19 -0500

    Part 1 completed

---
+++ script.js
@@ -0,0 +1,12 @@
+// Part 1: Fizz Buzz
+
+// Loop through all numbers from 1 to 100.
+    for (let i = 0; i < 101; i++){
+        console.log(i)
+
+    }
+
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
